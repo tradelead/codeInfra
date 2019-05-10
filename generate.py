8adc1dd54cf4ff49f38db4de9b663448d4f44316
calebmcelroy <calebmcelroy@gmail.com>
2019-05-09 21:27:42 -0400

    cloudformation: fix invalid cidr

--- generate.py
+++ generate.py
@@ -68,7 +68,7 @@ region = Ref("AWS::Region")
 availZoneA = Join("", [region, 'a'])
 availZoneB = Join("", [region, 'b'])
 pubSnCidr = '172.31.0.0/20'
-pubSnBCidr = '172.33.0.0/20'
+pubSnBCidr = '172.1.0.0/20'
 snCidr = '172.31.16.0/20'
 snCidrB = '172.31.32.0/20'
 
